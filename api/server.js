0d8fd4e95b58264bebefe5bdcd7a619247a197a3
Josh Z <jzieger10@gmail.com>
2019-01-09 16:28:56 -0500

    Added route to pull up a specific user posts

--- api/server.js
+++ api/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
-const userDb = require('../data/helpers/userDb');
-const postDb = require('../data/helpers/postDb');
+const userDb = require("../data/helpers/userDb");
+const postDb = require("../data/helpers/postDb");
 const server = express();
 
 server.use(express.json());
@@ -13,49 +13,56 @@ server.use(morgan("short"));
 
 // Custom middleware
 function uppercaseName(name) {
-    return name.toUpperCase();
+	return name.toUpperCase();
 }
 
 // Users
-server.get('/users', (req, res) => {
-    userDb.get()
-    .then(users => {
-        res.status(200).json({users})
-    })
-    .catch(res.status(500))
-})
+server.get("/users", (req, res) => {
+	userDb
+		.get()
+		.then(users => {
+			res.status(200).json({ users });
+		})
+		.catch(res.status(500));
+});
 
-server.post('/users', (req, res) => {
-    newUser = req.body;
-    userDb.insert(newUser).then(users => {
-        res.status(201).json({users})
-    })
-})
+server.get("/users/:id", (req, res) => {
+	const id = req.params.id;
+	userDb.getUserPosts(id).then(userPosts => {
+		res.status(201).json({ userPosts });
+	});
+});
 
-server.put('/users/:id', (req, res) => {
-    const id = req.params.id;
-    const changes = req.body;
-    userDb.update(id, changes).then(count => {
-        res.status(201).json({count})
-    })
-})
+server.post("/users", (req, res) => {
+	newUser = req.body;
+	userDb.insert(newUser).then(users => {
+		res.status(201).json({ users });
+	});
+});
 
-server.delete('/users/:id', (req, res) => {
-    const id = req.params.id;
-    userDb.remove(id).then(count => {
-        res.status(200).json({count})
-    })
-})
+server.put("/users/:id", (req, res) => {
+	const id = req.params.id;
+	const changes = req.body;
+	userDb.update(id, changes).then(count => {
+		res.status(201).json({ count });
+	});
+});
+
+server.delete("/users/:id", (req, res) => {
+	const id = req.params.id;
+	userDb.remove(id).then(count => {
+		res.status(200).json({ count });
+	});
+});
 
 // Posts
-server.get('/posts', (req, res) => {
-    postDb.get()
-    .then(posts => {
-        res.status(200).json({posts})
-    })
-    .catch(res.status(500))
-})
-
-
+server.get("/posts", (req, res) => {
+	postDb
+		.get()
+		.then(posts => {
+			res.status(200).json({ posts });
+		})
+		.catch(res.status(500));
+});
 
 module.exports = server;
